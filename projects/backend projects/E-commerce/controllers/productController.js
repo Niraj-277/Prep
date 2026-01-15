4397d08c475c1adee0f9382e113be359844c47ad
Niraj <nirajrampal217@gmail.com>
2026-01-15 23:58:24 +0530

    routing file

--- projects/backend projects/E-commerce/controllers/productController.js
+++ projects/backend projects/E-commerce/controllers/productController.js
@@ -2,7 +2,7 @@ const Product=require('../models/product');
 
 //@desc Get all products
 //@route Get /api/v1/products
-exports.getProduct=async(req , res ,next)=>{
+exports.getProducts=async(req , res ,next)=>{
     try{
         const products=await Product.find();
         res.status(200).json({
@@ -15,3 +15,62 @@ exports.getProduct=async(req , res ,next)=>{
     }
 }
 
+
+//@desc Update product 
+//@route PUT /api/v1/products/:id
+exports.updateProduct =async(req,res,next)=>{
+    try{
+        const product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+            new:true,
+            runValidators:true
+        });
+
+        if(!product){
+            return res.status(404).json({success:false,message:"no prodct found"});
+        }
+
+        res.status(200).json({success:true,data:product})
+    }catch(err){
+        next(err);
+        }
+}
+
+
+//@dest Create a product 
+//@route POST /api/v1/products/
+
+exports.createProduct=async(req,res,next)=>{
+    try{
+        const product = await Product.create(req.body);
+
+        res.status(201).json({
+            success:true,
+            data:product 
+        });
+    }catch(err){
+        next(err);
+    }
+}
+
+
+exports.deleteProduct=async(req,res,next)=>{
+    try{
+        const product =await Product.findByIdAndDelete(req.params.id,req.body,{
+            new:true,
+            runValidators:true
+        });
+
+        if(!product){
+            return res.status(404).json({
+                succes:false,
+                message:'product not found'
+            })
+        }
+        res.status(200).json({
+            success:true,
+            message:'Product deleted'
+        });
+    }catch(err){
+        next(err)    
+    }
+}
--- projects/backend projects/E-commerce/routes/productRoutes.js
+++ projects/backend projects/E-commerce/routes/productRoutes.js
@@ -4,84 +4,99 @@ const Product = require('../models/product');
 
 //use router instead of apps
 
-//!Getting the products
+//!Import the functions from teh controller 
+const {
+    getProducts,
+    createProduct,
+    updateProduct,
+    deleteProduct
+}=require('../controllers/productController')
 
-router.get('/',async(req,res)=>{
-    try{
-        const products= await Product.find();
-        res.status(200).json({
-            status:'success',
-            message:"here are the products",
-            count:products.length,
-            data:products
-        })
-    }catch(error){
-        res.status(404).json({
-            status:'fail',
-            error:error.message
-        })
-    }
-    
-})
+//2.connect the path to the function
+router.route('/').get(getProducts).post(createProduct);
 
-//!posting a product in the database
-
-router.post('/',async(req,res)=>{
-    try{
-        const product=await Product.create(req.body);
-
-        res.status(200).json({
-            status:'success',
-            message:"product got inserted",
-            data:product
-        })
-    }catch(error){
-        res.status(404).json({
-            status:'failure',
-            error:error.message
-        })
-    }
-})
-
-//! update the product Route
-
-router.put('/:id',async(req,res)=>{
-    try{
-        const product=await Product.findByIdAndUpdate(req.params.id,req.body,{
-            new:true,
-            runValidators:true
-        });
-        if(!product){
-            return res.status(404).json({success:false,message:"producct not found"})
-        }
-        res.status(200).json({success:true,data:product})
-    }catch (error) {
-        res.status(400).json({ success: false, error: error.message });
-    }
-
-})
-
-//! Delete a product Route
-
-router.delete('/:id',async(req,res)=>{
-    try{
-        const product=await Product.findByIdAndDelete(req.params.id);
-
-        if(!product){
-            return res.status(404).json({
-                status:'fail',
-                message:'Product not found '
-            })
-        }
-         res.status(200).json({
-                status:'success',
-                message:'Product Delete' })
-    }catch(error){
-        res.status(400).json({
-                status:'false',
-                message:'Product not found ',
-            error:error.message});
-    }
-})
+router.route('/:id').put(updateProduct).delete(deleteProduct);
 
 module.exports=router;
+
+// //!Getting the products
+
+// router.get('/',async(req,res)=>{
+//     try{
+//         const products= await Product.find();
+//         res.status(200).json({
+//             status:'success',
+//             message:"here are the products",
+//             count:products.length,
+//             data:products
+//         })
+//     }catch(error){
+//         res.status(404).json({
+//             status:'fail',
+//             error:error.message
+//         })
+//     }
+    
+// })
+
+// //!posting a product in the database
+
+// router.post('/',async(req,res)=>{
+//     try{
+//         const product=await Product.create(req.body);
+
+//         res.status(200).json({
+//             status:'success',
+//             message:"product got inserted",
+//             data:product
+//         })
+//     }catch(error){
+//         res.status(404).json({
+//             status:'failure',
+//             error:error.message
+//         })
+//     }
+// })
+
+// //! update the product Route
+
+// router.put('/:id',async(req,res)=>{
+//     try{
+//         const product=await Product.findByIdAndUpdate(req.params.id,req.body,{
+//             new:true,
+//             runValidators:true
+//         });
+//         if(!product){
+//             return res.status(404).json({success:false,message:"producct not found"})
+//         }
+//         res.status(200).json({success:true,data:product})
+//     }catch (error) {
+//         res.status(400).json({ success: false, error: error.message });
+//     }
+
+// })
+
+// //! Delete a product Route
+
+// router.delete('/:id',async(req,res)=>{
+//     try{
+//         const product=await Product.findByIdAndDelete(req.params.id);
+
+//         if(!product){
+//             return res.status(404).json({
+//                 status:'fail',
+//                 message:'Product not found '
+//             })
+//         }
+//          res.status(200).json({
+//                 status:'success',
+//                 message:'Product Delete' })
+//     }catch(error){
+//         res.status(400).json({
+//                 status:'false',
+//                 message:'Product not found ',
+//             error:error.message});
+//     }
+// })
+
+// module.exports=router;
